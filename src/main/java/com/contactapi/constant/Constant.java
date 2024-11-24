e296f2d60f87ced510a15e225f4b1b83c417a80c
Vighnesh Tathare <v42069@yahoo.com>
2024-11-24 20:37:59 +0530

    add

--- src/main/java/com/contactapi/constant/Constant.java
+++ src/main/java/com/contactapi/constant/Constant.java
@@ -2,6 +2,6 @@ package com.contactapi.constant;
 
 public class Constant {
 	
-	public static final String PHOTO_DIRECTORY=System.getProperty("user.home")+"/Downloads/uploads";
+	public static final String PHOTO_DIRECTORY=System.getProperty("user.home")+"/Downloads/uploads/";
 
 }
--- src/main/java/com/contactapi/controller/ContactController.java
+++ src/main/java/com/contactapi/controller/ContactController.java
@@ -1,20 +1,28 @@
 package com.contactapi.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.service.annotation.PutExchange;
 
+import com.contactapi.constant.Constant;
 import com.contactapi.entity.Contact;
 import com.contactapi.service.ContactServiceImplementation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 
 @RestController
@@ -38,12 +46,23 @@ public class ContactController {
 	public Contact createContact(@RequestBody Contact contact) {
 		return contactService.createContact(contact);
 	}
-	@PutExchange("/image")
+	@PutMapping("/image")
 	public String insertImage(@RequestParam String id,@RequestParam("file")MultipartFile file) {
 		//TODO: process POST request
 		
 		return contactService.uploadPhoto(id, file);
 	}
+	
+	@GetMapping(path = "/image/{filename}", produces = { IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE })
+    public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(Constant.PHOTO_DIRECTORY + filename));
+    }
+	
+	@GetMapping("/getcontact")
+	public Contact getMethodName(@RequestParam String id) {
+		return contactService.getContact(id);
+	}
+	
 	
 	
 	
