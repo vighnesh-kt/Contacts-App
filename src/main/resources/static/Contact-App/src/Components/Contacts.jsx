34401fb0f097646f5dc180b4196f1e5c8b7bdca4
Vighnesh Tathare <v42069@yahoo.com>
2024-11-26 00:01:28 +0530

    add

--- src/main/resources/static/Contact-App/src/Components/Contacts.jsx
+++ src/main/resources/static/Contact-App/src/Components/Contacts.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Contacts = () => {
+const Contacts = ({contact}) => {
   return (
-    <a
-      className="contactitem"
+    <Link to={`/contacts/${contact.id}`}
+      className="contact_item"
       href=""
     >
       <div className="contact_header">
-        <img src="" alt="" />
+        <img src={contact.photoUrl} alt={contact.name} />
       </div>
       <div className="contact_details" >
-        <p className="contact_name"></p>
-        <p className="contact_name"></p>
+        <p className="contact_name">{contact.name}</p>
+        <p className="contact_title">{contact.title}</p>
       </div>
       <div>
-        <p><i className="bi bi-envelope"></i>Email</p>
-        <p></p>
+        <p><i className="bi bi-envelope"></i>{contact.email}</p>
+        <p><i className="bi bi-geo"></i>{contact.address}</p>
+        <p><i className="bi bi-telephone"></i>{contact.phone}</p>
+        <p>{contact.status === 'Active' ?<i  className='bi bi-check-circle'></i>:
+        <i className="bi bi-x-circle"></i>
+        }{contact.status}</p>
       </div>
-    </a>
+    </Link>
   );
 };
 
